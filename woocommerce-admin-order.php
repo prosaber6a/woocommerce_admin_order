da8eb01a2e86cd8398c701dab248f306a6f26ec9
Saber Hossen Rabbani <saber.mizay@gmail.com>
2021-09-01 03:23:08 +0600

    shortcode done

--- woocommerce-admin-order.php
+++ woocommerce-admin-order.php
@@ -254,3 +254,156 @@ add_action('wp_ajax_woo_admin_order_submit', function () {
         echo json_encode(['success' => 'Successfully order inserted']);
     }
 });
+
+function woo_sku_order_shortcode () {
+
+
+    if (!is_integer(is_user_logged_in()) && !current_user_can('manage_woocommerce')) return;
+
+    woo_admin_order_assets('toplevel_page_woo-admin-order');
+    ?>
+
+    <div class="wrap">
+        <h1 class="wp-heading-inline">Create an Order</h1>
+
+        <div class="container flex justify-center">
+
+
+            <?php
+
+            if (!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))):
+                ?>
+                <h1 class="text-red">Please setup your woocommerce store</h1>
+            <?php
+            else:
+
+                global $woocommerce;
+                $countries_obj = new WC_Countries();
+                $countries = $countries_obj->__get('countries');
+
+                $products = wc_get_products([
+                    'status' => ['publish'],
+                    'return' => 'objects',
+                    'paginate' => false,
+                    'limit' => -1,
+                    'stock_status' => 'instock',
+                ]);
+
+
+
+                $products_arr = [];
+                foreach ($products as $product){
+                    $_thumbnail = wp_get_attachment_image_url($product->image_id, 'thumbnail');
+                    $_product_arr['id'] = $product->id;
+                    $_product_arr['img'] = $_thumbnail;
+                    $_product_arr['price'] = $product->price;
+                    $_product_arr['name'] = $product->name;
+                    $_product_arr['sku'] = $product->sku;
+
+                    $products_arr[] = $_product_arr;
+                }
+
+                wp_localize_script('woocommerce-admin-order-js', 'products', json_encode($products_arr));
+                wp_localize_script('woocommerce-admin-order-js', 'action_url', json_encode([admin_url('admin-ajax.php')]));
+
+
+
+                ?>
+
+                <div class="w-2/4 bg-white p-3">
+
+                    <form action="<?php echo admin_url('admin-post.php'); ?>" method="post" id="form">
+
+                        <?php
+                        wp_nonce_field('woo_admin_order_nonce', 'nonce');
+                        ?>
+
+
+                        <input type="text" id="product-sku-search" autocomplete="off"  placeholder="select product by sku" class=" w-full rounded mb-6">
+                        <div class="w-full relative hidden" id="search-container">
+                            <ul id="search-result" class="absolute z-20 overflow-y-scroll overflow-x-hidden h-56 bg-white w-full border border-black shadow-2xl -mt-7">
+
+                            </ul>
+                        </div>
+
+
+
+
+
+                        <table id="orderitems_table" class="table-auto w-full">
+                            <thead>
+                            <tr class="border-2 border-black border-l-0 border-r-0 border-t-0 mb-1">
+                                <th>Item</th>
+                                <th>Quantity</th>
+                                <th>Price</th>
+                                <th>Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                            <tfoot>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <th>Total: <span id="total_price">$0</span></th>
+                            </tr>
+                            </tfoot>
+                        </table>
+
+                        <h2 class="border-b-2 border-gray-400 mb-4 mt-12 text-2xl text-center">Customer</h2>
+                        <div class="grid grid-cols-2 gap-2">
+                            <div>
+                                <input type="text" id="cust_first_name" class="w-full" placeholder="First Name">
+                            </div>
+                            <div>
+                                <input type="text" id="cust_last_name" class="w-full" placeholder="Last Name">
+                            </div>
+                        </div>
+
+                        <div class="grid grid-cols-2 gap-2 mt-4">
+                            <div>
+                                <input type="text" id="cust_email" class="w-full" placeholder="Email">
+                            </div>
+                            <div>
+                                <input type="text" id="cust_phone" class="w-full" placeholder="Phone">
+                            </div>
+                        </div>
+
+
+                        <h2 class="border-b-2 border-gray-400 mb-4 mt-12 text-2xl text-center">Address</h2>
+                        <div class="grid grid-cols-2 gap-2">
+                            <div>
+                                <input type="text" id="address_line" class="w-full" placeholder="Address Line">
+                            </div>
+                            <div>
+                                <input type="text" id="city" class="w-full" placeholder="City">
+                            </div>
+                        </div>
+                        <div class="mt-2 text-center">
+
+                            <select id="country" class="w-full">
+                                <option value="" disabled selected>Select country</option>
+                                <?php foreach ($countries as $key => $value): ?>
+                                    <option value="<?php echo $key; ?>"><?php echo $value; ?></option>
+                                <?php endforeach; ?>
+                            </select>
+
+                        </div>
+                        <div class="mt-4 text-center">
+                            <button type="button" id="submit"
+                                    class="px-6 py-4 bg-indigo-400 text-white text-xl hover:bg-indigo-800">Save Order
+                            </button>
+                        </div>
+                    </form>
+                </div>
+
+            <?php endif; ?>
+
+
+        </div>
+    </div>
+    <?php
+}
+
+add_shortcode('woo_sku_order', 'woo_sku_order_shortcode');
