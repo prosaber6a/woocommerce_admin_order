ec01c8f9fa82ff99c635767b4bcb7bbd5eade5e6
Saber Hossen Rabbani <saber.miazy@gmail.com>
2021-09-01 14:33:17 +0600

    product duplicate issue solve

--- assets/js/woocommerce-admin-order.js
+++ assets/js/woocommerce-admin-order.js
@@ -1,7 +1,7 @@
 var orderItems = [];
 
 var url = JSON.parse(action_url)[0];
-console.log(url)
+
 const productData = JSON.parse(products);
 ;(function ($) {
     $(document).ready(function () {
@@ -102,7 +102,7 @@ const productData = JSON.parse(products);
 
 
                 data += `
-                <li onclick="renderTable(${arr.id})" class="hover:bg-blue-500 hover:text-white p-2">
+                <li onclick="itemInTable(${arr.id})" class="hover:bg-blue-500 hover:text-white p-2">
                                     
                     <div class="flex gap-2">
                         <img class="w-16" src="${arr.img}" alt="">
@@ -135,72 +135,95 @@ function remove_row(element) {
     let id = parseInt(element.parentNode.parentElement.getAttribute('data-id'));
 
     orderItems = orderItems.filter((item) => {
-        console.log(item)
         return item.product_id != id;
     });
 
-    console.log(orderItems)
 
     element.parentNode.parentElement.remove();
     calculate();
 }
 
-
-function renderTable(id = -1) {
-    console.log(id);
+function itemInTable(id = -1) {
 
     id = parseInt(id);
 
-    for (let i = 0; i < productData.length; i++) {
+    let i;
+    for (i = 0; i < orderItems.length; i++) {
+        if (orderItems[i].product_id === id) {
+            orderItems[i].quantity++;
+            renderTable();
+            return;
+        }
+    }
+
+    let obj = null;
+
+    for (i = 0; i < productData.length; i++) {
         if (productData[i].id === id) {
             obj = productData[i];
         }
     }
 
-
     if (obj === null) {
         return;
     }
 
-    let item = {product_id: obj.id, quantity: 1, price: obj.price, name: obj.name};
+    let item = {product_id: obj.id, quantity: 1, price: obj.price, name: obj.name, img: obj.img};
 
     orderItems[orderItems.length] = item;
 
-    let data = "";
-    data += `
-        <tr data-id="${obj.id}" class="border-b-2 border-gray-300 mb-1">
+    renderTable();
+
+}
+
+
+function renderTable() {
+
+    if (orderItems.length > 0) {
+
+
+        let data = "";
+
+        orderItems.forEach((obj) => {
+
+
+            data += `
+        <tr data-id="${obj.product_id}" class="border-b-2 border-gray-300 mb-1">
             <td class="flex gap-4 items-center m-2"><img class="h-12 border-0" src="${obj.img}"> <span class="block">${obj.name}</span></td>
             <td class="text-center">
-                <input type="number" name="quantity" onchange="update_quantity(this)" value="1" class="w-16">
+                <input type="number" name="quantity" data-id="${obj.product_id}" onchange="update_quantity(this)" value="${obj.quantity}" class="w-16">
             </td>
-            <td class="text-center">$ ${obj.price}</td>
+            <td class="text-center" data-id="${obj.id}">$ ${obj.price}</td>
             <td class="text-center">
                 <button type="delete" onclick="remove_row(this)" class="deletebtn hover:text-red-700 text-red-500">Delete</button>
             </td>
         </tr>
     `;
-    jQuery('#search-container').addClass('hidden')
-    jQuery('#orderitems_table tbody').append(data);
-    calculate();
+
+        });
+        jQuery('#search-container').addClass('hidden')
+        jQuery('#orderitems_table tbody').html(data);
+        calculate();
+
+    }
+
+    return;
 
 
 }
 
 
 function update_quantity(element) {
-    // console.log('hello')
     let qty = parseInt(element.value);
     let id = parseInt(element.parentNode.parentElement.getAttribute('data-id'));
     for (let i = 0; i < orderItems.length; i++) {
-        console.log(orderItems[i]);
         if (orderItems[i].product_id === id) {
             orderItems[i].quantity = qty;
             break;
         }
     }
 
-
-    calculate();
+    renderTable();
 }
 
 function calculate() {
